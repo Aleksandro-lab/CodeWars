4929fc6acbec67bb8626c32b07bf2625b2ce1a95
Alex <Alealekso@yandex.ru>
2020-10-16 17:23:04 +0300

    Решение задачи на подсчет суммы чисел кратных трем и пяти в заданном интервале

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNum = 10;
+            int cnt = 0;
+            for (int i = 3; i < maxNum; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    cnt = cnt + i;
+                }
+            }
+            Console.WriteLine(cnt);
+            Console.ReadLine();
+        }
+    }
+}
