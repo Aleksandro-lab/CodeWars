9e61c47c74c1ab58a77ba8064a86334fe603284d
Alex <Alealekso@yandex.ru>
2020-10-17 21:45:22 +0300

    dasd

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {1, 2, 3, 4};
+            int[] b = new int[] {1, 2, 3};
+            List<int> list = new List<int>();
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                list.Add(a[i]);                
+            }
+            
+            for (int i = 0; i < b.Length; i++)
+            {
+                list.RemoveAll(b[i]);
+                
+                
+            }
+
+            Console.WriteLine(0);
+            Console.ReadLine();
+        }
+    }
+}
